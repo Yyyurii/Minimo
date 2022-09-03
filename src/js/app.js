0690fc6462ad722bfa08286109e81aa504f70b09
Yyyurii <yurii.kryvko@gmail.com>
2022-09-03 13:14:13 +0300

    Created section Header

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,15 @@ import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
 
-/*
-import Swiper, { Navigation, Pagination } from 'swiper';
-const swiper = new Swiper();
-*/
+const burgerMenu = document.querySelector(".burger-menu");
+burgerMenu.addEventListener("click", onBurgerMenu);
+
+function onBurgerMenu(e) {
+  e.preventDefault();
+  const nav = document.querySelector(".nav");
+  const navList = document.querySelector(".nav__list");
+  const menuBox = document.querySelector("#menu_checkbox");
+  nav.classList.toggle("active");
+  navList.classList.toggle("active");
+  menuBox.classList.toggle("active");
+}
